695826fd4c0a25210f41519b32a4a1404c433268
Riki <ricardocr987@gmail.com>
2023-04-23 18:30:38 +0200

    improve profile page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -48,7 +48,7 @@ const Profile = () => {
                             <p className="text-black text-2xl text-center whitespace-normal">{updatedAuthor.username}</p>
                             </div>
                         </div>
-                        <div className="py-5 md:py-10 px-10 mb-5 flex justify-center items-center">
+                        <div className="py-5 md:py-10 px-10 mb-5 mt-5 flex justify-center items-center border rounded-lg">
                             {isEditing ?
                                 <form onSubmit={handleSubmit} className="space-y-2">
                                     <div className="mb-2">
@@ -59,7 +59,7 @@ const Profile = () => {
                                             id="username"
                                             value={updatedAuthor.username}
                                             onChange={handleInputChange}
-                                            className="border-gray-400 border-2 rounded-md px-3 py-2 w-full"
+                                            className="border-gray-400 border-2 rounded-md px-2 py-1 w-full"
                                         />
                                     </div>
                                     <div className="mb-2">
@@ -69,7 +69,17 @@ const Profile = () => {
                                             id="bio"
                                             value={updatedAuthor.bio}
                                             onChange={handleInputChange}
-                                            className="border-gray-400 border-2 rounded-md px-3 py-2 w-full h-18"
+                                            className="border-gray-400 border-2 rounded-md px-2 py-1 w-full h-18"
+                                        />
+                                    </div>
+                                    <div className="mb-2">
+                                        <label htmlFor="uri" className="text-black font-bold block mb-2">Image</label>
+                                        <input
+                                            name="uri"
+                                            id="uri"
+                                            value={updatedAuthor.uri}
+                                            onChange={handleInputChange}
+                                            className="border-gray-400 border-2 rounded-md px-2 py-1 w-full h-18"
                                         />
                                     </div>
                                     <div className="flex justify-between">
@@ -78,12 +88,12 @@ const Profile = () => {
                                     </div>
                                 </form>
                             :
-                                <div className="space-y-4 w-full">
-                                    <div className="mb-2">
+                                <div className="w-full">
+                                    <div className="mb-4 min-h-18">
                                         <p className="text-black font-bold text-lg">Bio:</p>
                                         <p className="text-black text-base max-w-full break-words">{updatedAuthor.bio}</p>
                                     </div>
-                                    <div className="mb-2">
+                                    <div className="mb-4">
                                         <p className="text-black font-bold text-lg">Created at:</p>
                                         <p className="text-black text-base max-w-full break-words">{moment(updatedAuthor.createdAt).format('MMM DD, YYYY')}</p>
                                     </div>
@@ -94,9 +104,12 @@ const Profile = () => {
                             }
                         </div>
                     </div>
-                    <div className="w-full h-56 bg-white rounded-lg shadow-md">
-                        <div className="py-10 px-10">
-                            <p className="inline align-middle text-black ml-2 text-5xl">Dashboard</p>
+                    <div className="container mx-auto px-10 grid grid-rows-1 bg-white rounded-lg shadow-md mb-4">
+                        <div className="py-5 px-10 flex justify-center items-center">
+                            <p className="text-black text-5xl">Dashboard</p>
+                        </div>
+                        <div className="py-5 h-96 border rounded mb-4">
+                            <p className="text-black text-5xl"></p>
                         </div>
                     </div>
                 </div>
